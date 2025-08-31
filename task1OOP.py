a0c24d6d4b9fe8f630b12df6a2a0a038e6983f85
Hamdoun-0 <mohamedhmhtech@gmail.com>
2025-08-31 21:29:15 +0300

    created main function

---
+++ task1OOP.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+if __name__ == '__main__' :
+    
+    name='John Smith'
+    
+    family={
+        'son':  {
+            'insured':True,
+            'Age':16
+                },
+
+        'Wife': {
+            'insured':False,
+            'Age':32
+                }
+            }
+    
